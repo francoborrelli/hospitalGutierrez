08dda9eaf762d130779cb41328a86b1deae35dc5
Pedro Brost <pedro.brost97@gmail.com>
2017-10-07 00:10:52 -0300

    add Controller abstract class

---
+++ Core/Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Core;
+
+abstract class Controller
+{
+
+    private $route_params = [];
+
+    public function __construct($route_params)
+    {
+        $this->route_params = $route_params;
+    }
+
+    public function __call($name, $args)
+    {
+        $method = $name . 'Action';
+
+        if (method_exists($this, $method)) {
+            if ($this->before() !== false) {
+                call_user_func_array([$this, $method], $args);
+                $this->after();
+            }
+        } else
+           echo "El método $method no se encontró en el controlador " . get_class($this); 
+    }
+
+    protected function before()
+    {
+    }
+
+    protected function after()
+    {
+    }
+
+}
