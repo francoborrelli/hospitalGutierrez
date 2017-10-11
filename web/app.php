66a637653b9da62e841d8c0461ae567a55ca5482
Pedro Brost <pedro.brost97@gmail.com>
2017-10-11 13:14:18 -0300

    Fix dynamic router paths

--- web/app.php
+++ web/app.php
@@ -5,6 +5,6 @@ require __DIR__.'/../vendor/autoload.php';
 $router = new Core\Router();
 
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-//$router->add('{controller}/{action}');
+$router->add('{controller}/{action}');
 
 $router->dispatch($_SERVER['QUERY_STRING']);
