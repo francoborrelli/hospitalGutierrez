ee36afe1b42926d525b87eafbf4e3275cd1221bf
Pedro Brost <pedro.brost97@gmail.com>
2017-10-11 14:44:42 -0300

    Add users route

--- web/app.php
+++ web/app.php
@@ -5,6 +5,7 @@ require __DIR__.'/../vendor/autoload.php';
 $router = new Core\Router();
 
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('{controller}/{action}');
+$router->add('admin/{controller}', ['action' => 'show']);
+//$router->add('{controller}/{action}');
 
 $router->dispatch($_SERVER['QUERY_STRING']);
