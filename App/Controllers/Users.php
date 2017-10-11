7ba53497eea33a3ad6c050556c388b7c72a54dd1
Pedro Brost <pedro.brost97@gmail.com>
2017-10-11 16:02:28 -0300

    Add new action users controller

--- App/Controllers/Users.php
+++ App/Controllers/Users.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use App\Models\User;
 
 class Users extends Controller
 {
@@ -10,6 +11,23 @@ class Users extends Controller
     public function showAction()
     {
         $this->render('Users/usersTable.html.twig');
+    }
+
+    public function newAction()
+    {
+        $user = new User();
+
+        $user->setEmail($_POST['email']);
+        $user->setFirstName($_POST['firstName']);
+        $user->setLastName($_POST['lastName']);
+        $user->setUsername($_POST['username']);
+        $user->setPassword('test');
+
+        $em = $this->getEntityManager();
+        $em->persist($user);
+        $em->flush();
+
+        $this->render('Home/index.html.twig');
     }
 
 }
