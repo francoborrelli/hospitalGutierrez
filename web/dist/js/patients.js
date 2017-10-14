9fb1215003412e1650fca3f570ef0072fa8fb85f
francoborrelli <francoborrelli96@outlook.com>
2017-10-14 15:25:39 -0300

    Delete details from index patients

--- web/dist/js/patients.js
+++ web/dist/js/patients.js
@@ -10,41 +10,9 @@ var oTable =  $('#patients').DataTable({
 
             //Renderiza la tabla de datos del usuario en el modal de detalles.
 
-            renderer: function ( api, rowIdx, columns ) {
-                    var first = '<table class="table"><br>'
-                    columns.slice(0,8).forEach(function ( col, i ) {
-                            first += ('<tr data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
-                                '<td>'+col.title+':'+'</td> '+
-                                '<td>'+col.data+'</td>'+
-                            '</tr>')
-                    } )
-                    first += "</table>";
-
-                    var second = '<table class="table"><br>'
-                    columns.slice(9,15).forEach(function ( col, i ) {
-                            second += ('<tr data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
-                                '<td>'+col.title+':'+'</td> '+
-                                '<td>'+col.data+'</td>'+
-                            '</tr>')
-                    } )
-                    second += "</div></table>";
-
-                    var myvar = '<ul class="nav nav-tabs" id="myTab" role="tablist">'+
-                    '                <li class="nav-item">'+
-                    '                  <a class="nav-link active" id="info-tab" data-toggle="tab" href="#info" role="tab" aria-controls="info" aria-expanded="true">Datos Personales</a>'+
-                    '                </li>'+
-                    '                <li class="nav-item">'+
-                    '                  <a class="nav-link" id="demo-tab" data-toggle="tab" href="#demo" role="tab" aria-controls="demo">Datos Demograficos</a>'+
-                    '                </li>'+
-                    '              </ul>'+
-                    '              <div class="tab-content" id="myTabContent">'+
-                    '                <div class="tab-pane fade show active" id="info" role="tabpanel" aria-labelledby="info-tab"><table class"table">' + first +
-                                    '</table></div>'+
-                    '                <div class="tab-pane fade" id="demo" role="tabpanel" aria-labelledby="demo-tab">' + second +'</div>'+
-                    '             </div>';
-                    console.log(first)
-                    return myvar
-                }
+            renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+                tableClass: 'table'
+            } )
         }
     },
     columns: [
@@ -52,17 +20,6 @@ var oTable =  $('#patients').DataTable({
         { responsivePriority: 2 }, // documento
         { responsivePriority: 4 }, //nombre
         { responsivePriority: 4 }, //apellido
-        { responsivePriority: 6 }, //nacimiento
-        { responsivePriority: 3 }, //Genero
-        { responsivePriority: 7 }, //Domicilio
-        { responsivePriority: 8 }, //Tel
-        { responsivePriority: 5 }, //Obra social
-        { responsivePriority: 10 }, //heladera
-        { responsivePriority: 10 }, //Electricidad
-        { responsivePriority: 10 }, //Mascotas
-        { responsivePriority: 10 }, //Vivienda
-        { responsivePriority: 10 }, //calefaccion
-        { responsivePriority: 10 }, //Agua
         { responsivePriority: 1 }   //Opciones
     ],
     "order": [
@@ -70,7 +27,7 @@ var oTable =  $('#patients').DataTable({
     ],
     "aoColumnDefs": [{
         "bSortable": false,
-        "aTargets": [6]
+        "aTargets": [4]
     }],
     "bLengthChange": false,
     "info": false,
