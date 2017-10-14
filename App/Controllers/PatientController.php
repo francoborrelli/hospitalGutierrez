0904241f8546416251b7343363d288538465df90
Pedro Brost <pedro.brost97@gmail.com>
2017-10-14 16:37:26 -0300

    Add new patient feature

--- App/Controllers/PatientController.php
+++ App/Controllers/PatientController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use App\Models\Patient;
 
 class PatientController extends Controller
 {
@@ -10,6 +11,31 @@ class PatientController extends Controller
     public function indexAction()
     {
         $this->render('Patients/patientsTable.html.twig');
+    }
+
+    public function newAction()
+    {
+        $em = $this->getEntityManager();
+        $userRepository = $em->getRepository(Patient::class);
+
+        $data = $this->getPatientData($_POST);
+        $patient = new Patient($data);
+        $validationErrors = $patient->validationErrors();
+        if (empty($validationErrors)){
+            $em->persist($patient);
+            $em->flush();
+
+            $this->addFlashMessage('success', '¡Felicitaciones!', 'Se ha agregado al paciente correctamente');
+            $this->redirect('/patients');
+        } else {
+            $this->render('Patients/patientsTable.html.twig');
+        }
+    }
+
+    private function getPatientData($data)
+    {
+        $em = $this->getEntityManager();
+         
     }
     
 }
