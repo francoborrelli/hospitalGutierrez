5bfac8fc03351e7cbd9bf6035b45730540d56eb7
Pedro Brost <pedro.brost97@gmail.com>
2017-10-15 11:08:24 -0300

    Add config to the view

--- App/Controllers/ConfigController.php
+++ App/Controllers/ConfigController.php
@@ -3,13 +3,16 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use App\Models\Site;
 
 class ConfigController extends Controller
 {
 
     public function indexAction()
     {
-        $this->render('Config/config.html.twig');
+        $siteRepository = $this->getEntityManager()->getRepository(Site::class);
+        $site = $siteRepository->findAll()[0];
+        $this->render('Config/config.html.twig', ['site' => $site]);
     }
 
 }
