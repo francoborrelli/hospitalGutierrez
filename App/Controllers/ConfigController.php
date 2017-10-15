0f4cbc34c2ac95b85992840a9984bd26c3cb2068
francoborrelli <francoborrelli96@outlook.com>
2017-10-15 15:42:03 -0300

    Add config serverside validations

--- App/Controllers/ConfigController.php
+++ App/Controllers/ConfigController.php
@@ -16,11 +16,20 @@ class ConfigController extends Controller
     public function updateAction()
     {
         $site = $this->getSite();
-        $site->setData($_POST);
-        $this->getEntityManager()->flush();
+ 
+        $validationSite = clone $site;
+        $validationSite->setData($_POST);
 
-        $this->addFlashMessage('success', 'Felicitaciones!.', 'Los datos del sitio se han actualizado correctamente');
-        $this->redirect('/admin/config');
+        $validationErrors = $validationSite->validationErrors();
+        if (empty($validationErrors)){           
+            $site->setData($_POST);
+            $this->getEntityManager()->flush();
+
+            $this->addFlashMessage('success', 'Felicitaciones!.', 'Los datos del sitio se han actualizado correctamente');
+            $this->redirect('/admin/config');
+        }else{
+            $this->render('Config/config.html.twig', ['errors' => $validationErrors, 'validationSite' => $validationSite]);
+        }    
     }
 
 }
