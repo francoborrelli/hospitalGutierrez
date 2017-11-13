1edfb0ab824c1267b574c042c19966b2b27e7c1c
Pedro Brost <pedro.brost97@gmail.com>
2017-11-13 16:13:04 -0300

    Improve speed patients curl

--- App/Repositories/DocumentTypeRepository.php
+++ App/Repositories/DocumentTypeRepository.php
@@ -8,38 +8,30 @@ use App\Models\DocumentType;
 class DocumentTypeRepository extends APIrepository
 {
 
-    private static $docTypes = null;
+    private static $docTypes = [];
 
     public static function findAll()
     {
-        if (is_null(self::$docTypes)) {
+        if (empty(self::$docTypes)) {
             $array = json_decode(self::get('https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento'), true);
-            $docTypes = [];
             foreach ($array as $docType) {
-                $docTypes[] = new DocumentType($docType['id'], $docType['nombre']);
+                self::$docTypes[] = new DocumentType($docType['id'], $docType['nombre']);
             }
-            self::$docTypes = $docTypes;
-            return $docTypes;
-        } else {
-            return self::$docTypes;
         }
+        return self::$docTypes;
     }
 
     public static function find($id)
     {
-        if (is_null(self::$docTypes)) {
-            self::findAll();
-        }
-        $result = null;
         foreach(self::$docTypes as $docType) {
             if ($docType->getId() == $id) {
-                $result = $docType;
-                break;
+                return $docType;
             }
         }
-        return $result;
-        //$docType = json_decode(self::get('https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento/'.$id), true);
-        //return new DocumentType($docType['id'], $docType['nombre']);
+        $docType = json_decode(self::get('https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-documento/'.$id), true);
+        $docType =  new DocumentType($docType['id'], $docType['nombre']);
+        self::$docTypes[] = $docType;
+        return $docType;
     }
 
 }
