f5050182c099c3ece8d854302a58640d2d7d5c91
francoborrelli <francoborrelli96@outlook.com>
2017-11-17 13:52:56 -0300

    Add findRecords

--- App/Repositories/ClinicalRecordRepository.php
+++ App/Repositories/ClinicalRecordRepository.php
@@ -7,5 +7,32 @@ use Doctrine\ORM\EntityRepository;
 class ClinicalRecordRepository extends EntityRepository
 {
 
+    public function patientClinicalRecords($patient)
+    {
+
+        $qb = $this->createQueryBuilder('p');
+        
+        $qb->select('c')
+        ->from('App\Models\ClinicalRecord','c')
+        ->andWhere('c.deleted = :state')
+        ->setParameter('state', false)
+        ->andWhere('c.patient = :patient')
+        ->setParameter('patient', $patient->getId());  
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function PatientCount($patient)
+    {
+        $qb = $this->createQueryBuilder('p');
+        
+        $qb->select('Max(c.id)')
+        ->from('App\Models\ClinicalRecord','c')
+        ->where('c.patient = :patient')
+        ->setParameter('patient', $patient->getId());  
+
+        return $qb->getQuery()->getSingleScalarResult();
+    }
+
 
 }
