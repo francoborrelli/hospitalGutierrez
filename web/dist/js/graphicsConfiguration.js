3b8162c2cfe2aa15f97acd638dc93dd248e8ab77
francoborrelli <francoborrelli96@outlook.com>
2017-11-17 20:34:47 -0300

    Add graphicsConfiguration.js

---
+++ web/dist/js/graphicsConfiguration.js
@@ -0,0 +1,22 @@
+
+// Pone mensajes de Highchart en español
+
+Highcharts.setOptions({
+    lang: {
+        decimalPoint:',',
+        downloadJPEG: 'Descargar como JPEG',
+        downloadPDF: 'Descargar como PDF',
+        downloadPNG: 'Descargar como PNG',
+        downloadSVG: 'Descargar como SVG',
+        downloadXLS: 'Descargar como XLS',
+        loading:'Cargando...',
+        noData:'No hay datos para mostrar',
+        printChart: 'Imprimir Gráfico',
+        resetZoom: 'Resetear zoom'
+    }
+});
+
+// Deshabilita la opción print
+
+var theExportOptions = Highcharts.getOptions().exporting.buttons.contextButton.menuItems;
+theExportOptions.splice(0, 2);
