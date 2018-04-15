8310ba20373a130346f9aba96774d25e4181ab58
francoborrelli <francoborrelli96@outlook.com>
2018-04-15 12:15:33 -0300

    Improve login form

--- client/src/scenes/Login/Login.js
+++ client/src/scenes/Login/Login.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import LoginForm from './LoginForm'
-import { Card } from 'antd';
+import React, {Component} from "react";
+import LoginForm from "./components/loginForm"
+import { Card, message } from "antd";
 
 const styles = {
   width: 350,
   margin: "100px auto 40px"
 }
 
-const login = (props) => {
+class Login extends Component {
+  state = {
+    enterLoading: false
+  }
+
+  submitHandler = e => {
+      this.setState({ enterLoading: true })
+      this.props.history.push("/")
+      message.success("Sesión iniciada correctamente.", 3)
+      this.setState({ enterLoading: false })
+    }
+
+  render(){
     return (
-        <div className="container loginPage">
-            <Card title="Iniciar Sesión" style={styles}>
-                <LoginForm/>
-            </Card>
-        </div>);
+      <div className="container loginPage">
+          <Card title="Iniciar Sesión" style={styles}>
+              <LoginForm submitted={this.submitHandler} loading={this.props.enterLoading}/>
+          </Card>
+      </div>);
+  }
 }
 
-export default login;
+export default Login;
