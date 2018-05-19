f17e7259720e9c0175402b01f3da7ebdc8f2fa0f
Pedro Brost <pedro.brost97@gmail.com>
2018-05-19 19:49:51 -0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,72 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const compress = require('compression');
+const methodOverride = require('method-override');
+const httpStatus = require('http-status');
+const expressWinston = require('express-winston');
+const expressValidation = require('express-validation');
+const helmet = require('helmet');
+const winstonInstance = require('../config/winston');
+const routes = require('./index.route');
+const config = require('../config/config');
+const APIError = require('./helpers/APIError');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo');
+if (config.env === 'development') {
+  app.use(logger('dev'));
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(compress());
+app.use(methodOverride());
+
+app.use(helmet());
+
+if (config.env === 'development') {
+  expressWinston.requestWhitelist.push('body');
+  expressWinston.responseWhitelist.push('body');
+  app.use(expressWinston.logger({
+    winstonInstance,
+    meta: true,
+    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+    colorStatus: true
+  }));
+}
+
+app.use('/', routes);
+
+app.use((err, req, res, next) => {
+  if (err instanceof expressValidation.ValidationError) {
+    const unifiedErrorMessage = err.errors.map(error => error.messages.join('. ')).join(' and ');
+    const error = new APIError(unifiedErrorMessage, err.status, true);
+    return next(error);
+  } else if (!(err instanceof APIError)) {
+    const apiError = new APIError(err.message, err.status, err.isPublic);
+    return next(apiError);
+  }
+  return next(err);
 });
+
+app.use((req, res, next) => {
+  const err = new APIError('API not found', httpStatus.NOT_FOUND);
+  return next(err);
+});
+
+if (config.env !== 'test') {
+  app.use(expressWinston.errorLogger({
+    winstonInstance
+  }));
+}
+
+app.use((err, req, res, next) =>
+  res.status(err.status).json({
+    message: err.isPublic ? err.message : httpStatus[err.status],
+    stack: config.env === 'development' ? err.stack : {}
+  })
+);
 
 module.exports = app;
