bf875f5cdd957c2449ccd45e67cf98a20fabca35
Pedro Brost <pedro.brost97@gmail.com>
2018-05-19 18:14:09 -0300

    Connect to mongo with mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const mongoose = require('mongoose');
+const util = require('util');
 
-const app = express();
+const config = require('./config/config');
+const app = require('./src/app');
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo');
+const debug = require('debug')('hospital-api:index');
+
+const mongoUri = config.mongo.host;
+mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
 });
 
-app.listen(8080);
+if (config.mongooseDebug) {
+  mongoose.set('debug', (collectionName, method, query, doc) => {
+    debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+  });
+}
+
+app.listen(config.port, () => {
+  console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
+});
+
+module.exports = app;
