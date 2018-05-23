13cc4e6b10dde2f7ac1464bfe7957c1274c02750
Pedro Brost <pedro.brost97@gmail.com>
2018-05-23 17:21:51 -0300

    Implement user login

--- server/src/auth/auth.controller.js
+++ server/src/auth/auth.controller.js
@@ -1,46 +1,30 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const httpStatus = require('http-status');
 const APIError = require('../helpers/APIError');
 const config = require('../../config/config');
+const User = require('../user/user.model');
 
-// sample user, used for authentication
-const user = {
-  username: 'pedro',
-  password: 'pass'
-};
-
-/**
- * Returns jwt token if valid username and password is provided
- * @param req
- * @param res
- * @param next
- * @returns {*}
- */
-function login(req, res, next) {
-  // Ideally you'll fetch this from the db
-  // Idea here was to show how jwt works with simplicity
-  if (req.body.username === user.username && req.body.password === user.password) {
-    const token = jwt.sign({
-      username: user.username
-    }, config.jwtSecret);
-    return res.json({
-      token,
-      username: user.username
-    });
-  }
-
-  const err = new APIError('Authentication error', httpStatus.UNAUTHORIZED, true);
-  return next(err);
+function getAuthError() {
+  return new APIError('Authentication error', httpStatus.UNAUTHORIZED, true);
 }
 
-/**
- * This is a protected route. Will return random number only if jwt token is provided in header.
- * @param req
- * @param res
- * @returns {*}
- */
+async function login(req, res, next) {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return next(getAuthError());
+
+  const match = await bcrypt.compare(req.body.password, user.password);
+
+  if (match) {
+    const token = jwt.sign({ username: user.username }, config.jwtSecret);
+    return res.json({ token });
+  }
+
+  return next(getAuthError());
+}
+
 function getRandomNumber(req, res) {
-  // req.user is assigned by jwt middleware if valid token is provided
   return res.json({
     user: req.user,
     num: Math.random() * 100
