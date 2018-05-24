037b6b8fbde15747b5582c69be1e29345c42e487
Pedro Brost <pedro.brost97@gmail.com>
2018-05-24 00:23:36 -0300

    Add auth action creators

---
+++ client/src/store/actions/auth.js
@@ -0,0 +1,37 @@
+import axios from '../../axios-api';
+import * as types from './types';
+import jwtDecode from 'jwt-decode';
+
+const authStart = () => ({
+  type: types.AUTH_START
+});
+
+const authSuccess = jwt => {
+  const user = jwtDecode(jwt.token);
+  return {
+    type: types.AUTH_SUCCESS,
+    jwt: jwt.token,
+    user
+  };
+};
+
+const authFail = error => ({
+  type: types.AUTH_FAIL,
+  error: error.response
+});
+
+export const login = (email, password) => {
+  return async dispatch => {
+    dispatch(authStart());
+    try {
+      const response = await axios.post('/auth/login', { email, password });
+      dispatch(authSuccess(response.data));
+    } catch (error) {
+      dispatch(authFail(error));
+    }
+  };
+};
+
+export const seeSuccessMessage = () => ({
+  type: types.AUTH_SEE_MESSAGE
+});
