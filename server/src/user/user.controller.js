d514ddf2d74b5ff4ab8499aa349236c7d64a3933
Pedro Brost <pedro.brost97@gmail.com>
2018-05-26 00:16:52 -0300

    Populate roles in user list

--- server/src/user/user.controller.js
+++ server/src/user/user.controller.js
@@ -78,12 +78,6 @@ function update(req, res, next) {
     .catch(e => next(e));
 }
 
-/**
- * Get user list.
- * @property {number} req.query.skip - Number of users to be skipped.
- * @property {number} req.query.limit - Limit number of users to be returned.
- * @returns {User[]}
- */
 function list(req, res, next) {
   const { limit = 50, skip = 0 } = req.query;
   User.list({ limit, skip })
@@ -91,10 +85,6 @@ function list(req, res, next) {
     .catch(e => next(e));
 }
 
-/**
- * Delete user.
- * @returns {User}
- */
 function remove(req, res, next) {
   const user = req.user;
   user
--- server/src/user/user.model.js
+++ server/src/user/user.model.js
@@ -96,6 +96,7 @@ UserSchema.statics = {
 
   list({ skip = 0, limit = 50 } = {}) {
     return this.find()
+      .populate({ path: 'roles', select: 'name' })
       .sort({ createdAt: -1 })
       .skip(+skip)
       .limit(+limit)
