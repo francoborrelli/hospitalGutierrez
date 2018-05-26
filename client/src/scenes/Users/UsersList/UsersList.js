f5d417bbf5af1043894400c0e7f6ffee5fdf8be3
Pedro Brost <pedro.brost97@gmail.com>
2018-05-26 00:17:01 -0300

    Add user list to client

--- client/src/scenes/Users/UsersList/UsersList.js
+++ client/src/scenes/Users/UsersList/UsersList.js
@@ -1,68 +1,73 @@
-import React, { Component } from "react"
-import {Col, message } from "antd"
+import React, { Component } from 'react';
+import { Col, message } from 'antd';
 import Row from '../../../components/grid/row';
-import Section from "../../../components/header/sectionHeader/sectionHeader"
-import SearchForm from "./components/searchForm"
-import Table from "./components/table"
+import Section from '../../../components/header/sectionHeader/sectionHeader';
+import SearchForm from './components/searchForm';
+import Table from './components/table';
 
+import axios from '../../../axios-api';
 
 class UserList extends Component {
   state = {
     loading: true,
     searching: false,
-    data: []
-  }
+    users: []
+  };
 
-  componentDidMount = () => {
-    //Get table data
-
-    this.setState({ loading: false })
-  }
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get('/users');
+      this.setState({ loading: false, users: response.data });
+    } catch (error) {
+      message.error('Ocurrió un error al obtener los usuarios');
+    }
+  };
 
   searchHandler = data => {
-    this.setState({ searching: true })
+    this.setState({ searching: true });
 
     //Search request
 
-    this.setState({ searching: false })
-  }
+    this.setState({ searching: false });
+  };
 
   deleteUserHandler = user => {
     return new Promise((resolve, reject) => {
       //Change Timeout for delelte request
-      setTimeout(Math.random() > 0.3 ? resolve : reject, 1000)
+      setTimeout(Math.random() > 0.3 ? resolve : reject, 1000);
     })
       .then(() => {
-        this.setState({ loading: true })
-        const name = user.name + " " + user.lastname
-        message.success("Se eliminó a " + name + " correctamente.")
-        this.setState({ loading: false })
+        this.setState({ loading: true });
+        const name = user.name + ' ' + user.lastname;
+        message.success('Se eliminó a ' + name + ' correctamente.');
+        this.setState({ loading: false });
       })
-      .catch(() => message.error("Algo falló. Intentá nuevamente."))
-  }
+      .catch(() => message.error('Algo falló. Intentá nuevamente.'));
+  };
 
   render() {
     return (
       <Section title="usuarios">
         <Row>
           <Col xl={7}>
-              <SearchForm
-                loading={this.state.searching}
-                documentTypes={this.state.documentTypes}
-                submitted={this.searchHandler}
-              />
+            <SearchForm
+              loading={this.state.searching}
+              documentTypes={this.state.documentTypes}
+              submitted={this.searchHandler}
+            />
           </Col>
           <Col xl={17}>
-              <Table
-                loading={this.state.loading}
-                onDelete={this.deleteUserHandler}
-                addPath="/users/add"
-              />
+            <Table
+              loading={this.state.loading}
+              onDelete={this.deleteUserHandler}
+              addPath="/users/add"
+              data={this.state.users}
+            />
           </Col>
         </Row>
       </Section>
-    )
+    );
   }
 }
 
-export default UserList
+export default UserList;
--- client/src/scenes/Users/UsersList/components/table.js
+++ client/src/scenes/Users/UsersList/components/table.js
@@ -1,90 +1,74 @@
-import React from "react"
-import {Button, Icon, Card} from "antd"
-import Dropdown from "./dropdown"
-import {Link} from "react-router-dom"
+import React from 'react';
+import { Button, Icon, Card } from 'antd';
+import Dropdown from './dropdown';
+import { Link } from 'react-router-dom';
 import Table from '../../../../components/table/table';
 
-const data = [
-  {
-    key: "1",
-    name: "Franco",
-    lastname: "Borrelli",
-    username: "fborrelli",
-    email: "FrancoBorrelli@gmai.com",
-    role: "Administrador",
-    status: "Activo"
-  }, {
-    key: "2",
-    name: "Pedro",
-    lastname: "Brost",
-    username: "2",
-    email: "pedrobrost@gmail.com",
-    role: "",
-    status: "Activo"
-  }, {
-    key: "3",
-    name: "Juan",
-    lastname: "Perez",
-    documentType: "1",
-    email: 397872,
-    role: "",
-    status: "Bloqueado"
-  }, {
-    key: "4",
-    name: "John",
-    lastname: "Garcia",
-    username: "3",
-    email: 41243,
-    role: "",
-    status: "Activo"
-  }
-]
-
 const tableUsers = props => {
+  const data = props.data.map(user => ({
+    key: user._id,
+    name: user.firstName,
+    lastname: user.lastName,
+    username: user.username,
+    email: user.email,
+    role: user.roles.map(role => role.name).join(', '),
+    status: user.active ? 'Activo' : 'Bloqueado'
+  }));
+
   const columns = [
     {
-      title: "Nombre",
-      dataIndex: "name",
-      key: "name",
+      title: 'Nombre',
+      dataIndex: 'name',
+      key: 'name',
       sorter: (a, b) => a.name < b.name
-    }, {
-      title: "Apellido",
-      dataIndex: "lastname",
-      key: "lastname",
+    },
+    {
+      title: 'Apellido',
+      dataIndex: 'lastname',
+      key: 'lastname',
       sorter: (a, b) => a.lastname < b.lastname
-    }, {
-      title: "Nombre de Usuario",
-      dataIndex: "username",
-      key: "username",
+    },
+    {
+      title: 'Nombre de Usuario',
+      dataIndex: 'username',
+      key: 'username',
       sorter: (a, b) => a.username < b.username
-    }, {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
       sorter: (a, b) => a.email < b.email
-    }, {
-      title: "Roles",
-      dataIndex: "role",
-      key: "role"
-    }, {
-      title: "Estado",
-      dataIndex: "status",
-      key: "status"
-    }, {
-      title: "Acciones",
-      key: "action",
-      align: "center",
-      fixed: "right",
+    },
+    {
+      title: 'Roles',
+      dataIndex: 'role',
+      key: 'role'
+    },
+    {
+      title: 'Estado',
+      dataIndex: 'status',
+      key: 'status'
+    },
+    {
+      title: 'Acciones',
+      key: 'action',
+      align: 'center',
+      fixed: 'right',
       width: 120,
-      render: (text, record) => (<Dropdown record={record} onDelete={props.onDelete}/>)
+      render: (text, record) => (
+        <Dropdown record={record} onDelete={props.onDelete} />
+      )
     }
-  ]
+  ];
 
-  const extra = <Link to={props.addPath}>
-    <Button onClick={props.onAdd}>
-      <Icon type="user-add"/>Agregar
-    </Button>
-  </Link>
+  const extra = (
+    <Link to={props.addPath}>
+      <Button onClick={props.onAdd}>
+        <Icon type="user-add" />Agregar
+      </Button>
+    </Link>
+  );
 
   return (
     <Card extra={extra}>
@@ -92,9 +76,10 @@ const tableUsers = props => {
         columns={columns}
         dataSource={data}
         loading={props.loading}
-        scroll={{x: 950}}/>
+        scroll={{ x: 950 }}
+      />
     </Card>
-  )
-}
+  );
+};
 
-export default tableUsers
+export default tableUsers;
