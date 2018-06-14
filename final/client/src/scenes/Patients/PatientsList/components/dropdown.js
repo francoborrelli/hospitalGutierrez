080cd0d1aa2a6cd71af47b056b490262fa6a85a6
Franco Borrelli <francoborrelli96@outlook.com>
2018-06-14 20:03:41 -0300

    Add addRecord button

--- final/client/src/scenes/Patients/PatientsList/components/dropdown.js
+++ final/client/src/scenes/Patients/PatientsList/components/dropdown.js
@@ -1,61 +1,57 @@
-import React from "react"
-import {Menu} from "antd"
-import Dropdown from "../../../../components/dropdown/dropdown"
-import {deleteModal as Modal} from "../../../../components/modal/modal"
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Menu } from 'antd';
+import Dropdown from '../../../../components/dropdown/dropdown';
+import { deleteModal as Modal } from '../../../../components/modal/modal';
+import { Link } from 'react-router-dom';
 
-const Item = Menu.Item
-const Divider = Menu.Divider
+const Item = Menu.Item;
+const Divider = Menu.Divider;
+
 
 const dropdown = props => {
+  const show = props.user.permissions.includes('paciente_show') ? (
+    <Item key="0">
+      <Link to={'patient/' + props.record.key}>Ver Perfil</Link>
+    </Item>
+  ) : null;
 
-  const show = props
-    .user
-    .permissions
-    .includes('paciente_show')
-    ? <Item key="0">
-        <Link to={"patient/" + props.record.key}>
-          Ver Perfil
-        </Link>
-      </Item>
-    : null
+  const clinic = props.user.permissions.includes('control_new') ? (
+    <Item key="6">
+      <Link to={'patient/' + props.record.key + '/addRecord'}>Agregar Control</Link>
+    </Item>
+  ) : null;
 
-  const edit = props
-    .user
-    .permissions
-    .includes('paciente_update')
-    ? <Item key="1">
-        <Link to={"patient/" + props.record.key + "/edit"}>
-          Editar
-        </Link>
-      </Item>
-    : null
+  const edit = props.user.permissions.includes('paciente_update') ? (
+    <Item key="1">
+      <Link to={'patient/' + props.record.key + '/edit'}>Editar</Link>
+    </Item>
+  ) : null;
 
-  const destroy = props
-    .user
-    .permissions
-    .includes('paciente_destroy')
-    ? <Item key="2">
-        <a
-          onClick={() => Modal("paciente", props.record, () => props.onDelete(props.record))}>
-          Eliminar
-        </a>
-      </Item>
-    : null
+  const destroy = props.user.permissions.includes('paciente_destroy') ? (
+    <Item key="2">
+      <a
+        onClick={() =>
+          Modal('paciente', props.record, () => props.onDelete(props.record))
+        }
+      >
+        Eliminar
+      </a>
+    </Item>
+  ) : null;
 
-  const menu = <Menu>
-    {show}
-    {show && edit
-      ? <Divider/>
-      : null}
-    {edit}
-    {edit && destroy
-      ? <Divider/>
-      : null}
-    {destroy}
-  </Menu>
+  const menu = (
+    <Menu>
+      {show}
+      {show && clinic ? <Divider /> : null}
+      {clinic}
+      {clinic && edit ? <Divider /> : null}
+      {edit}
+      {edit && destroy ? <Divider /> : null}
+      {destroy}
+    </Menu>
+  );
 
-  return (<Dropdown menu={menu}/>)
-}
+  return <Dropdown menu={menu} />;
+};
 
-export default dropdown
+export default dropdown;
