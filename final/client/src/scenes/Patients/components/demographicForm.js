aeb8829cb7e998e8c6e14f79d577d7ca72cf1a80
Franco Borrelli <francoborrelli96@outlook.com>
2018-06-14 18:19:52 -0300

    Improve phone validation

--- final/client/src/scenes/Patients/components/demographicForm.js
+++ final/client/src/scenes/Patients/components/demographicForm.js
@@ -1,147 +1,163 @@
-import React from "react"
-import Form from "../../../containers/Form/Form"
+import React from 'react';
+import Form from '../../../containers/Form/Form';
 
 const demographicForm = props => {
   const fields = {
     houseType: {
-      name: "houseType",
-      label: "Tipo de Vivienda",
-      type: "select",
+      name: 'houseType',
+      label: 'Tipo de Vivienda',
+      type: 'select',
       props: {
-        placeholder: "Tipo de Vivienda"
+        placeholder: 'Tipo de Vivienda'
       },
       options: props.data.houseTypes || [],
       rules: [
         {
           required: true,
-          message: "Seleccione una opción"
+          message: 'Seleccione una opción'
         }
       ]
     },
     heatingType: {
-      name: "heatingType",
-      label: "Tipo de Calefacción",
-      type: "select",
+      name: 'heatingType',
+      label: 'Tipo de Calefacción',
+      type: 'select',
       props: {
-        placeholder: "Tipo de Calefacción"
+        placeholder: 'Tipo de Calefacción'
       },
       options: props.data.heatingTypes || [],
       rules: [
         {
           required: true,
-          message: "Seleccione una opción"
+          message: 'Seleccione una opción'
         }
       ]
     },
     waterType: {
-      name: "waterType",
-      label: "Tipo de Agua",
-      type: "select",
+      name: 'waterType',
+      label: 'Tipo de Agua',
+      type: 'select',
       props: {
-        placeholder: "Tipo de Agua"
+        placeholder: 'Tipo de Agua'
       },
       options: props.data.waterTypes || [],
       rules: [
         {
           required: true,
-          message: "Seleccione una opción"
+          message: 'Seleccione una opción'
         }
       ]
     },
     fridge: {
-      name: "hasRefrigerator",
-      label: "¿Tiene Heladera?",
-      type: "select",
+      name: 'hasRefrigerator',
+      label: '¿Tiene Heladera?',
+      type: 'select',
       props: {
-        placeholder: "¿Tiene Heladera?"
+        placeholder: '¿Tiene Heladera?'
       },
       options: [
         {
           id: 1,
-          nombre: "Si"
-        }, {
+          nombre: 'Si'
+        },
+        {
           id: 0,
-          nombre: "No"
+          nombre: 'No'
         }
       ],
       rules: [
         {
           required: true,
-          message: "Seleccione una opción"
+          message: 'Seleccione una opción'
         }
       ]
     },
     electricity: {
-      name: "hasElectricity",
-      label: "¿Tiene Electricidad?",
-      type: "select",
+      name: 'hasElectricity',
+      label: '¿Tiene Electricidad?',
+      type: 'select',
       props: {
-        placeholder: "¿Tiene Electricidad?"
+        placeholder: '¿Tiene Electricidad?'
       },
       options: [
         {
           id: 1,
-          nombre: "Si"
-        }, {
+          nombre: 'Si'
+        },
+        {
           id: 0,
-          nombre: "No"
+          nombre: 'No'
         }
       ],
       rules: [
         {
           required: true,
-          message: "Seleccione una opción"
+          message: 'Seleccione una opción'
         }
       ]
     },
     pets: {
-      name: "hasPet",
-      label: "¿Tiene Mascotas?",
-      type: "select",
+      name: 'hasPet',
+      label: '¿Tiene Mascotas?',
+      type: 'select',
       props: {
-        placeholder: "¿Tiene Mascotas?"
+        placeholder: '¿Tiene Mascotas?'
       },
       options: [
         {
           id: 1,
-          nombre: "Si"
-        }, {
+          nombre: 'Si'
+        },
+        {
           id: 0,
-          nombre: "No"
+          nombre: 'No'
         }
       ],
       rules: [
         {
           required: true,
-          message: "Seleccione una opción"
+          message: 'Seleccione una opción'
         }
       ]
     }
-  }
-  let values={}
-  const patient = props.patient
+  };
+  let values = {};
+  const patient = props.patient;
   if (patient) {
     values = {
-      hasRefrigerator: patient.hasRefrigerator === undefined ? undefined : patient.hasRefrigerator ? 1 : 0,
+      hasRefrigerator:
+        patient.hasRefrigerator === undefined
+          ? undefined
+          : patient.hasRefrigerator
+            ? 1
+            : 0,
       hasPet: patient.hasPet === undefined ? undefined : patient.hasPet ? 1 : 0,
-      hasElectricity: patient.hasElectricity === undefined ? undefined : patient.hasElectricity ? 1 : 0,
-      heatingType: patient.heatingType ? patient.heatingType.toString() : undefined,
+      hasElectricity:
+        patient.hasElectricity === undefined
+          ? undefined
+          : patient.hasElectricity
+            ? 1
+            : 0,
+      heatingType: patient.heatingType
+        ? patient.heatingType.toString()
+        : undefined,
       houseType: patient.houseType ? patient.houseType.toString() : undefined,
-      waterType: patient.waterType ? patient.waterType.toString() : undefined,
-    }
+      waterType: patient.waterType ? patient.waterType.toString() : undefined
+    };
   }
 
-  return (<Form
-    fields={fields}
-    {...props}
-    layout="vertical"
-    buttonText={props.btnText
-    ? props.btnText
-    : "Confirmar"}
-    loading={props.loading}
-    onBack={props.prevStep}
-    defaultValues={values}
-    track={["waterType", "houseType", "heatingType"]}/>)
-}
+  return (
+    <Form
+      fields={fields}
+      {...props}
+      layout="vertical"
+      buttonText={props.btnText ? props.btnText : 'Confirmar'}
+      loading={props.loading}
+      onBack={props.prevStep}
+      defaultValues={values}
+      track={['waterType', 'houseType', 'heatingType']}
+    />
+  );
+};
 
-export default demographicForm
+export default demographicForm;
--- final/client/src/scenes/Patients/components/personalForm.js
+++ final/client/src/scenes/Patients/components/personalForm.js
@@ -1,214 +1,214 @@
-import React from "react"
-import Form from "../../../containers/Form/Form"
-import moment from "moment"
+import React from 'react';
+import Form from '../../../containers/Form/Form';
+import moment from 'moment';
 import axios from '../../../axios-api';
 
 const personalDataForm = props => {
-
   const documentValidator = (form, rule, documentNumber, callback) => {
-
-    const documentType = form.getFieldValue("documentType")
-    const patient = props.patient ? props.patient : {}
-    if (documentType && (parseInt(documentType, 10) !== patient.documentType || documentNumber !== patient.documentNumber)) {
+    const documentType = form.getFieldValue('documentType');
+    const patient = props.patient ? props.patient : {};
+    if (
+      documentType &&
+      (parseInt(documentType, 10) !== patient.documentType ||
+        documentNumber !== patient.documentNumber)
+    ) {
       axios
         .get('/patients/documentExists', {
-        params: {
-          documentNumber: documentNumber,
-          documentType: documentType,
-          id: patient
-            ? patient.id
-            : undefined
-        }
-      })
-        .then((response) => {
-          if (response.data) {
-            callback("El documento ya se encuentra registrado en el sistema")
-          } else {
-            callback()
+          params: {
+            documentNumber: documentNumber,
+            documentType: documentType,
+            id: patient ? patient.id : undefined
           }
         })
+        .then(response => {
+          if (response.data) {
+            callback('El documento ya se encuentra registrado en el sistema');
+          } else {
+            callback();
+          }
+        });
     } else {
-      callback()
+      callback();
     }
-  }
+  };
 
   const revalidate = (form, rule, documentType, callback) => {
-    const documentNumber = form.getFieldValue("documentNumber")
+    const documentNumber = form.getFieldValue('documentNumber');
     if (documentNumber) {
-      form.validateFields(["documentNumber"], {force: true})
+      form.validateFields(['documentNumber'], { force: true });
     }
-    callback()
-  }
+    callback();
+  };
 
   const fields = {
     lastName: {
-      name: "lastName",
-      label: "Apellido",
-      type: "input",
+      name: 'lastName',
+      label: 'Apellido',
+      type: 'input',
       rules: [
         {
           required: true,
           whitespace: true,
-          message: "Ingrese el apellido"
-        }, {
-          message: "Debe tener solo letras",
-          pattern: "^[a-zA-ZÀ-ÿ\u00f1\u00d1  _]*(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1" +
-              "  _]+$"
+          message: 'Ingrese el apellido'
+        },
+        {
+          message: 'Debe tener solo letras',
+          pattern:
+            '^[a-zA-ZÀ-ÿ\u00f1\u00d1  _]*(s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1' +
+            '  _]+$'
         }
       ]
     },
     firstName: {
-      name: "firstName",
-      label: "Nombre",
-      type: "input",
+      name: 'firstName',
+      label: 'Nombre',
+      type: 'input',
       rules: [
         {
           required: true,
           whitespace: true,
-          message: "Ingrese el nombre"
-        }, {
-          message: "Debe tener solo letras",
-          pattern: "^[a-zA-ZÀ-ÿ\u00f1\u00d1  _]*(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1" +
-              "  _]+$"
+          message: 'Ingrese el nombre'
+        },
+        {
+          message: 'Debe tener solo letras',
+          pattern:
+            '^[a-zA-ZÀ-ÿ\u00f1\u00d1  _]*(s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1' +
+            '  _]+$'
         }
       ]
     },
     document: {
-      name: "documentNumber",
-      type: "select-input",
-      label: "Documento",
+      name: 'documentNumber',
+      type: 'select-input',
+      label: 'Documento',
       rules: [
         {
           required: true,
           whitespace: true,
-          message: "Ingrese el documento"
+          message: 'Ingrese el documento'
         }
       ],
       customValidator: documentValidator,
       select: {
-        type: "select",
-        name: "documentType",
+        type: 'select',
+        name: 'documentType',
         props: {
           style: {
             width: 70
           },
-          placeholder: "Tipo",
+          placeholder: 'Tipo',
           required: true
         },
         customValidator: revalidate,
         options: props.data.documentTypes || []
       },
       input: {
-        type: "input",
-        label: "Número"
+        type: 'input',
+        label: 'Número'
       }
     },
     birthday: {
-      name: "birthday",
-      label: "Fecha de Nacimiento",
-      type: "datePicker",
+      name: 'birthday',
+      label: 'Fecha de Nacimiento',
+      type: 'datePicker',
       props: {
-        placeholder: "Fecha de Nacimiento"
+        placeholder: 'Fecha de Nacimiento'
       },
       rules: [
         {
-          type: "object",
+          type: 'object',
           required: true,
-          message: "Ingrese la fecha de nacimiento"
+          message: 'Ingrese la fecha de nacimiento'
         }
       ]
     },
     gender: {
-      name: "gender",
-      label: "Género",
-      type: "select",
+      name: 'gender',
+      label: 'Género',
+      type: 'select',
       props: {
-        placeholder: "Género"
+        placeholder: 'Género'
       },
       options: [
         {
-          id: "male",
-          nombre: "Másculino"
-        }, {
-          id: "female",
-          nombre: "Femenino"
+          id: 'male',
+          nombre: 'Másculino'
+        },
+        {
+          id: 'female',
+          nombre: 'Femenino'
         }
       ],
       rules: [
         {
           required: true,
-          message: "Seleccione un género"
+          message: 'Seleccione un género'
         }
       ]
     },
     address: {
-      name: "address",
-      label: "Dirección",
-      type: "input",
+      name: 'address',
+      label: 'Dirección',
+      type: 'input',
       rules: [
         {
           required: true,
           whitespace: true,
-          message: "Ingrese la dirección"
+          message: 'Ingrese la dirección'
         }
       ]
     },
     insurance: {
-      name: "insurance",
-      label: "Obras Social",
-      type: "select",
+      name: 'insurance',
+      label: 'Obras Social',
+      type: 'select',
       props: {
-        placeholder: "Obra Social",
+        placeholder: 'Obra Social',
         allowClear: true
       },
       options: props.data.insurances || []
     },
     phone: {
-      name: "phone",
-      label: "Teléfono",
-      type: "input",
+      name: 'phone',
+      label: 'Teléfono',
+      type: 'input',
       rules: [
         {
-          pattern: "^([+]{1})?[0-9 -/]*$",
-          message: "Ingrese un teléfono válido",
+          pattern: '^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{0,10}$',
+          message: 'Ingrese un teléfono válido'
         }
       ]
     }
-  }
+  };
 
   let values;
-  const patient = props.patient
+  const patient = props.patient;
   if (patient) {
     values = {
       documentType: patient.documentType
-        ? patient
-          .documentType
-          .toString()
+        ? patient.documentType.toString()
         : undefined,
       firstName: patient.firstName,
       lastName: patient.lastName,
       birthday: moment(patient.birthday),
       phone: patient.phone,
-      insurance: patient.insurance
-        ? patient
-          .insurance
-          .toString()
-        : undefined,
+      insurance: patient.insurance ? patient.insurance.toString() : undefined,
       address: patient.address,
       gender: patient.gender,
       documentNumber: patient.documentNumber
-    }
+    };
   }
 
-  return (<Form
-    fields={fields}
-    {...props}
-    layout="vertical"
-    buttonText={props.btnText
-    ? props.btnText
-    : "Confirmar"}
-    defaultValues={values}
-    track={["documentType"]}/>)
-}
+  return (
+    <Form
+      fields={fields}
+      {...props}
+      layout="vertical"
+      buttonText={props.btnText ? props.btnText : 'Confirmar'}
+      defaultValues={values}
+      track={['documentType']}
+    />
+  );
+};
 
-export default personalDataForm
+export default personalDataForm;
